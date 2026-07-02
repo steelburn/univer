3cbf4d0bb1b3f72b97d4bfb2d6c84da3a2698582
jikkai <jikkai@users.noreply.github.com>
2026-07-02 10:23:01 +0000

    Deploy preview for PR  🛫

---
+++ pr-preview/pr-7180/chunk-DBRQB4K6.js
@@ -0,0 +1,119 @@
+// ../packages/docs-drawing-ui/src/locale/zh-CN.ts
+var locale = {
+  "docs-drawing-ui": {
+    title: "\u56FE\u7247",
+    upload: {
+      float: "\u63D2\u5165\u56FE\u7247"
+    },
+    shape: {
+      insert: {
+        title: "\u63D2\u5165\u56FE\u5F62",
+        rectangle: "\u63D2\u5165\u77E9\u5F62",
+        ellipse: "\u63D2\u5165\u692D\u5706"
+      }
+    },
+    panel: {
+      title: "\u7F16\u8F91\u56FE\u7247"
+    },
+    "image-popup": {
+      replace: "\u66FF\u6362",
+      delete: "\u5220\u9664",
+      edit: "\u7F16\u8F91",
+      crop: "\u88C1\u526A",
+      reset: "\u91CD\u7F6E\u5927\u5C0F"
+    },
+    "image-text-wrap": {
+      title: "\u6587\u5B57\u73AF\u7ED5",
+      wrappingStyle: "\u73AF\u7ED5\u65B9\u5F0F",
+      square: "\u56DB\u5468\u578B",
+      topAndBottom: "\u4E0A\u4E0B\u578B",
+      inline: "\u5D4C\u5165\u578B",
+      behindText: "\u886C\u4E8E\u6587\u5B57\u4E0B\u65B9",
+      inFrontText: "\u6D6E\u4E8E\u6587\u5B57\u4E0A\u65B9",
+      wrapText: "\u81EA\u52A8\u6362\u884C",
+      bothSide: "\u4E24\u4FA7",
+      leftOnly: "\u5DE6\u4FA7",
+      rightOnly: "\u53F3\u4FA7",
+      distanceFromText: "\u8DDD\u6B63\u6587",
+      top: "\u4E0A\uFF08px\uFF09",
+      left: "\u5DE6\uFF08px\uFF09",
+      bottom: "\u4E0B\uFF08px\uFF09",
+      right: "\u53F3\uFF08px\uFF09"
+    },
+    "image-position": {
+      title: "\u4F4D\u7F6E",
+      horizontal: "\u6C34\u5E73",
+      vertical: "\u5782\u76F4",
+      absolutePosition: "\u7EDD\u5BF9\u4F4D\u7F6E\uFF08px\uFF09",
+      relativePosition: "\u76F8\u5BF9\u4F4D\u7F6E",
+      toTheRightOf: "\u53F3\u4FA7",
+      relativeTo: "\u76F8\u5BF9\u4E8E",
+      bellow: "\u4E0B\u65B9",
+      options: "\u9009\u9879",
+      moveObjectWithText: "\u5BF9\u8C61\u968F\u6587\u5B57\u79FB\u52A8",
+      column: "\u680F",
+      margin: "\u9875\u8FB9\u8DDD",
+      page: "\u9875\u9762",
+      line: "\u884C",
+      paragraph: "\u6BB5\u843D"
+    },
+    "update-status": {
+      exceedMaxSize: "\u56FE\u7247\u5927\u5C0F\u8D85\u8FC7\u9650\u5236, \u9650\u5236\u4E3A{0}M",
+      invalidImageType: "\u56FE\u7247\u7C7B\u578B\u9519\u8BEF",
+      exceedMaxCount: "\u56FE\u7247\u53EA\u80FD\u4E00\u6B21\u4E0A\u4F20{0}\u5F20",
+      invalidImage: "\u65E0\u6548\u56FE\u7247"
+    },
+    shortcut: {
+      "drawing-view": "\u7ED8\u56FE\u89C6\u56FE",
+      "drawing-move-down": "\u4E0B\u79FB\u7ED8\u56FE",
+      "drawing-move-up": "\u4E0A\u79FB\u7ED8\u56FE",
+      "drawing-move-left": "\u5DE6\u79FB\u7ED8\u56FE",
+      "drawing-move-right": "\u53F3\u79FB\u7ED8\u56FE",
+      "drawing-delete": "\u5220\u9664\u7ED8\u56FE"
+    }
+  }
+};
+var zh_CN_default = locale;
+
+// ../packages/docs-hyper-link-ui/src/locale/zh-CN.ts
+var locale2 = {
+  "docs-hyper-link-ui": {
+    edit: {
+      confirm: "\u786E\u8BA4",
+      cancel: "\u53D6\u6D88",
+      title: "\u94FE\u63A5",
+      address: "\u94FE\u63A5",
+      label: "\u6587\u672C",
+      labelError: "\u8BF7\u8F93\u5165\u94FE\u63A5\u6587\u672C",
+      placeholder: "\u8BF7\u8F93\u5165\u5408\u6CD5\u7684\u94FE\u63A5",
+      addressError: "\u8BF7\u8F93\u5165\u5408\u6CD5\u7684\u94FE\u63A5"
+    },
+    info: {
+      copy: "\u590D\u5236",
+      edit: "\u7F16\u8F91",
+      cancel: "\u53D6\u6D88\u94FE\u63A5",
+      coped: "\u94FE\u63A5\u5DF2\u88AB\u590D\u5236\u5230\u526A\u8D34\u677F"
+    },
+    menu: {
+      tooltip: "\u6DFB\u52A0\u94FE\u63A5"
+    }
+  }
+};
+var zh_CN_default2 = locale2;
+
+// ../packages/docs-thread-comment-ui/src/locale/zh-CN.ts
+var locale3 = {
+  "docs-thread-comment-ui": {
+    panel: {
+      title: "\u8BC4\u8BBA\u7BA1\u7406",
+      addComment: "\u6DFB\u52A0\u8BC4\u8BBA"
+    }
+  }
+};
+var zh_CN_default3 = locale3;
+
+export {
+  zh_CN_default,
+  zh_CN_default2,
+  zh_CN_default3
+};
---
+++ pr-preview/pr-7180/react16/lazy-VKUDMQXO.js
@@ -0,0 +1,63 @@
+import {
+  UniverDocsMentionUIPlugin
+} from "./chunk-LVCWDEZJ.js";
+import {
+  UniverSheetsNoteUIPlugin,
+  UniverSheetsTableUIPlugin
+} from "./chunk-M5I27MPO.js";
+import {
+  UniverSheetsConditionalFormattingUIPlugin,
+  UniverSheetsDataValidationUIPlugin,
+  UniverSheetsFilterUIPlugin
+} from "./chunk-UKWYKK3W.js";
+import {
+  UniverSheetsThreadCommentUIPlugin
+} from "./chunk-2YGIBCCL.js";
+import {
+  UniverSheetsNumfmtUIPlugin
+} from "./chunk-KPC3P2GJ.js";
+import {
+  UniverThreadCommentUIPlugin
+} from "./chunk-TVRJI4PO.js";
+import "./chunk-H6IKFNUB.js";
+import "./chunk-WEH6UA5C.js";
+import "./chunk-LBKVQ5E5.js";
+import "./chunk-TG5VI7XE.js";
+import {
+  UniverSheetsFormulaUIPlugin
+} from "./chunk-IXGZUTVM.js";
+import "./chunk-LSASRAKL.js";
+import {
+  UniverSheetsDrawingUIPlugin
+} from "./chunk-OYJGZT7S.js";
+import "./chunk-4RI3AED2.js";
+import "./chunk-NPMILPK4.js";
+import "./chunk-VMW4UVMR.js";
+import "./chunk-24TUDJHE.js";
+import "./chunk-PDLKZOO7.js";
+import "./chunk-IGRQIP3D.js";
+import "./chunk-MWE37INK.js";
+import "./chunk-5WHMUGXX.js";
+import "./chunk-TEI2QFPC.js";
+import "./chunk-EQ2B2W73.js";
+import "./chunk-HECJ2TYE.js";
+
+// src/sheets/lazy.ts
+function getLazyPlugins() {
+  return [
+    [UniverDocsMentionUIPlugin],
+    [UniverSheetsNumfmtUIPlugin],
+    [UniverThreadCommentUIPlugin],
+    [UniverSheetsThreadCommentUIPlugin],
+    [UniverSheetsNoteUIPlugin],
+    [UniverSheetsTableUIPlugin],
+    [UniverSheetsFormulaUIPlugin],
+    [UniverSheetsDataValidationUIPlugin],
+    [UniverSheetsConditionalFormattingUIPlugin],
+    [UniverSheetsFilterUIPlugin, { useRemoteFilterValuesGenerator: false }],
+    [UniverSheetsDrawingUIPlugin]
+  ];
+}
+export {
+  getLazyPlugins as default
+};
