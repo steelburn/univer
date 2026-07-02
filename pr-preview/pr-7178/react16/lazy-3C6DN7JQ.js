e61e8e09e5e68ee9a495402118fd0d8104a17cc9
wpxp123456 <wpxp123456@users.noreply.github.com>
2026-07-02 07:43:35 +0000

    Deploy preview for PR  🛫

---
+++ pr-preview/pr-7178/react16/lazy-3C6DN7JQ.js
@@ -0,0 +1,63 @@
+import {
+  UniverDocsMentionUIPlugin
+} from "./chunk-VCXF7MLD.js";
+import {
+  UniverSheetsNoteUIPlugin,
+  UniverSheetsTableUIPlugin
+} from "./chunk-FR7QKSFT.js";
+import {
+  UniverSheetsConditionalFormattingUIPlugin,
+  UniverSheetsDataValidationUIPlugin,
+  UniverSheetsFilterUIPlugin
+} from "./chunk-2EBJAR6J.js";
+import {
+  UniverSheetsThreadCommentUIPlugin
+} from "./chunk-SJSKUJUE.js";
+import {
+  UniverSheetsNumfmtUIPlugin
+} from "./chunk-VP5BSPDF.js";
+import {
+  UniverThreadCommentUIPlugin
+} from "./chunk-OVCGF6ZF.js";
+import "./chunk-H6IKFNUB.js";
+import "./chunk-W75Y7QEW.js";
+import "./chunk-LBKVQ5E5.js";
+import "./chunk-TG5VI7XE.js";
+import {
+  UniverSheetsFormulaUIPlugin
+} from "./chunk-62MBQRPS.js";
+import "./chunk-LSASRAKL.js";
+import {
+  UniverSheetsDrawingUIPlugin
+} from "./chunk-5YHPNAJD.js";
+import "./chunk-HCVYW3WN.js";
+import "./chunk-NPMILPK4.js";
+import "./chunk-VT6VSWAG.js";
+import "./chunk-5NAFVRJF.js";
+import "./chunk-LIG3MUKA.js";
+import "./chunk-IGRQIP3D.js";
+import "./chunk-MWE37INK.js";
+import "./chunk-5WHMUGXX.js";
+import "./chunk-TEI2QFPC.js";
+import "./chunk-EQ2B2W73.js";
+import "./chunk-HECJ2TYE.js";
+
+// src/sheets-no-worker/lazy.ts
+function getLazyPlugins() {
+  return [
+    [UniverDocsMentionUIPlugin],
+    [UniverSheetsNumfmtUIPlugin],
+    [UniverThreadCommentUIPlugin],
+    [UniverSheetsThreadCommentUIPlugin],
+    [UniverSheetsNoteUIPlugin],
+    [UniverSheetsTableUIPlugin],
+    [UniverSheetsFormulaUIPlugin],
+    [UniverSheetsDataValidationUIPlugin],
+    [UniverSheetsConditionalFormattingUIPlugin],
+    [UniverSheetsFilterUIPlugin, { useRemoteFilterValuesGenerator: false }],
+    [UniverSheetsDrawingUIPlugin]
+  ];
+}
+export {
+  getLazyPlugins as default
+};
---
+++ pr-preview/pr-7178/very-lazy-5LOPN23O.js
@@ -0,0 +1,86 @@
+import {
+  UniverActionRecorderPlugin
+} from "./chunk-BJHC3TSZ.js";
+import {
+  UniverUniscriptPlugin
+} from "./chunk-Y3TCBLZ7.js";
+import "./chunk-R7EK2BG5.js";
+import "./chunk-SQZXXC67.js";
+import "./chunk-KV3ZS2HR.js";
+import {
+  UniverSheetsFindReplacePlugin
+} from "./chunk-Z2A7BUGN.js";
+import {
+  UniverSheetsSortUIPlugin
+} from "./chunk-5T2G2M7C.js";
+import {
+  UniverSheetsCrosshairHighlightPlugin
+} from "./chunk-QTGVHGY2.js";
+import {
+  UniverDebuggerPlugin
+} from "./chunk-YKS5S7YB.js";
+import {
+  UniverWatermarkPlugin
+} from "./chunk-IPUQHYKH.js";
+import "./chunk-NPPGDXIO.js";
+import {
+  loadDebuggerLocale
+} from "./chunk-772ZZOFA.js";
+import "./chunk-4SREX2CE.js";
+import {
+  UniverSheetsHyperLinkUIPlugin
+} from "./chunk-2GZZ32Y6.js";
+import "./chunk-LBKVQ5E5.js";
+import "./chunk-62MBQRPS.js";
+import "./chunk-5YHPNAJD.js";
+import "./chunk-HCVYW3WN.js";
+import "./chunk-VT6VSWAG.js";
+import "./chunk-5NAFVRJF.js";
+import "./chunk-LIG3MUKA.js";
+import "./chunk-IGRQIP3D.js";
+import "./chunk-GVT4D57Q.js";
+import "./chunk-MWE37INK.js";
+import "./chunk-5WHMUGXX.js";
+import "./chunk-TEI2QFPC.js";
+import "./chunk-EQ2B2W73.js";
+import "./chunk-HECJ2TYE.js";
+
+// src/sheets-no-worker/very-lazy.ts
+var IS_E2E = false;
+function getVeryLazyPlugins() {
+  const plugins = [
+    [UniverActionRecorderPlugin],
+    [UniverSheetsHyperLinkUIPlugin],
+    [UniverSheetsSortUIPlugin],
+    [UniverSheetsCrosshairHighlightPlugin],
+    [UniverSheetsFindReplacePlugin],
+    [UniverWatermarkPlugin]
+  ];
+  if (!IS_E2E) {
+    plugins.push([UniverDebuggerPlugin, {
+      fabEntryUnitType: 2 /* UNIVER_SHEET */,
+      localeLoader: loadDebuggerLocale
+    }]);
+    plugins.push([UniverUniscriptPlugin, {
+      getWorkerUrl(_, label) {
+        if (label === "json") {
+          return "/vs/language/json/json.worker.js";
+        }
+        if (label === "css" || label === "scss" || label === "less") {
+          return "/vs/language/css/css.worker.js";
+        }
+        if (label === "html" || label === "handlebars" || label === "razor") {
+          return "/vs/language/html/html.worker.js";
+        }
+        if (label === "typescript" || label === "javascript") {
+          return "/vs/language/typescript/ts.worker.js";
+        }
+        return "/vs/editor/editor.worker.js";
+      }
+    }]);
+  }
+  return plugins;
+}
+export {
+  getVeryLazyPlugins as default
+};
